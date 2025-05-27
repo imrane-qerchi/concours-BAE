21901b2028931b79fbd42f59699e2dfb1f77ff13
Imrane QERCHI <150057046+imrane-qerchi@users.noreply.github.com>
2025-05-27 13:24:25 +0200

    Mise à jour des liens légaux dans le composant Footer et ajout des icônes des réseaux sociaux avec des liens externes.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -64,10 +64,12 @@ onBeforeUnmount(() => {
       <!-- Liens légaux -->
       <div class="text-sm text-center space-y-1">
         <div class="flex gap-3 flex-wrap justify-center">
-          <RouterLink to="/cgu" class="hover:underline">CGU</RouterLink>
+          <RouterLink to="/conditions-generales-d-utilisation" class="hover:underline"
+            >CGU</RouterLink
+          >
           <RouterLink to="/reglement" class="hover:underline">Règlement</RouterLink>
-          <RouterLink to="/mentions" class="hover:underline">Mentions légales</RouterLink>
-          <RouterLink to="/confidentialite" class="hover:underline">
+          <RouterLink to="/mentions-legales" class="hover:underline">Mentions légales</RouterLink>
+          <RouterLink to="/politique-de-confidentialite" class="hover:underline">
             Politique de confidentialité
           </RouterLink>
         </div>
@@ -75,10 +77,31 @@ onBeforeUnmount(() => {
           Contact :
           <a href="mailto:alumni@univ-fcomte.fr" class="underline">alumni@univ-fcomte.fr</a>
         </p>
-        <div class="flex justify-center gap-4 mt-2">
-          <a href="#" class="hover:opacity-80">LinkedIn</a>
-          <a href="#" class="hover:opacity-80">Instagram</a>
-          <a href="#" class="hover:opacity-80">Facebook</a>
+        <div class="flex justify-center gap-6 mt-5">
+          <a
+            href="https://www.linkedin.com/school/universite-de-franche-comte/"
+            target="_blank"
+            aria-label="LinkedIn"
+            class="hover:opacity-80"
+          >
+            <img src="/linkedin.svg" alt="LinkedIn" class="h-15 w-15" />
+          </a>
+          <a
+            href="https://www.instagram.com/fc_univ/"
+            target="_blank"
+            aria-label="Instagram"
+            class="hover:opacity-80"
+          >
+            <img src="/instagram.svg" alt="Instagram" class="h-15 w-15" />
+          </a>
+          <a
+            href="https://www.facebook.com/universiteFrancheComte/"
+            target="_blank"
+            aria-label="Facebook"
+            class="hover:opacity-80"
+          >
+            <img src="/facebook.svg" alt="Facebook" class="h-15 w-15" />
+          </a>
         </div>
         <p class="mt-4 text-gray-300">
           © Réseau Alumni de l'Université Marie et Louis Pasteur – 2025
