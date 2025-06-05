00085c4f0a71ec40c3b76a0f63c7c2ec752abe23
Imrane QERCHI <150057046+imrane-qerchi@users.noreply.github.com>
2025-06-05 15:06:37 +0200

    Correction des guillemets dans les fichiers CSS et TypeScript pour assurer la cohérence du style de code.

--- src/pocketbase-types.ts
+++ src/pocketbase-types.ts
@@ -1,18 +1,18 @@
 /**
-* This file was @generated using pocketbase-typegen
-*/
+ * This file was @generated using pocketbase-typegen
+ */
 
 import type PocketBase from 'pocketbase'
 import type { RecordService } from 'pocketbase'
 
 export enum Collections {
-	Authorigins = "_authOrigins",
-	Externalauths = "_externalAuths",
-	Mfas = "_mfas",
-	Otps = "_otps",
-	Superusers = "_superusers",
-	Participants = "participants",
-	Users = "users",
+  Authorigins = '_authOrigins',
+  Externalauths = '_externalAuths',
+  Mfas = '_mfas',
+  Otps = '_otps',
+  Superusers = '_superusers',
+  Participants = 'participants',
+  Users = 'users'
 }
 
 // Alias types for improved usability
@@ -21,138 +21,142 @@ export type RecordIdString = string
 export type HTMLString = string
 
 type ExpandType<T> = unknown extends T
-	? T extends unknown
-		? { expand?: unknown }
-		: { expand: T }
-	: { expand: T }
+  ? T extends unknown
+    ? { expand?: unknown }
+    : { expand: T }
+  : { expand: T }
 
 // System fields
 export type BaseSystemFields<T = unknown> = {
-	id: RecordIdString
-	collectionId: string
-	collectionName: Collections
+  id: RecordIdString
+  collectionId: string
+  collectionName: Collections
 } & ExpandType<T>
 
 export type AuthSystemFields<T = unknown> = {
-	email: string
-	emailVisibility: boolean
-	username: string
-	verified: boolean
+  email: string
+  emailVisibility: boolean
+  username: string
+  verified: boolean
 } & BaseSystemFields<T>
 
 // Record types for each collection
 
 export type AuthoriginsRecord = {
-	collectionRef: string
-	created?: IsoDateString
-	fingerprint: string
-	id: string
-	recordRef: string
-	updated?: IsoDateString
+  collectionRef: string
+  created?: IsoDateString
+  fingerprint: string
+  id: string
+  recordRef: string
+  updated?: IsoDateString
 }
 
 export type ExternalauthsRecord = {
-	collectionRef: string
-	created?: IsoDateString
-	id: string
-	provider: string
-	providerId: string
-	recordRef: string
-	updated?: IsoDateString
+  collectionRef: string
+  created?: IsoDateString
+  id: string
+  provider: string
+  providerId: string
+  recordRef: string
+  updated?: IsoDateString
 }
 
 export type MfasRecord = {
-	collectionRef: string
-	created?: IsoDateString
-	id: string
-	method: string
-	recordRef: string
-	updated?: IsoDateString
+  collectionRef: string
+  created?: IsoDateString
+  id: string
+  method: string
+  recordRef: string
+  updated?: IsoDateString
 }
 
 export type OtpsRecord = {
-	collectionRef: string
-	created?: IsoDateString
-	id: string
-	password: string
-	recordRef: string
-	sentTo?: string
-	updated?: IsoDateString
+  collectionRef: string
+  created?: IsoDateString
+  id: string
+  password: string
+  recordRef: string
+  sentTo?: string
+  updated?: IsoDateString
 }
 
 export type SuperusersRecord = {
-	created?: IsoDateString
-	email: string
-	emailVisibility?: boolean
-	id: string
-	password: string
-	tokenKey: string
-	updated?: IsoDateString
-	verified?: boolean
+  created?: IsoDateString
+  email: string
+  emailVisibility?: boolean
+  id: string
+  password: string
+  tokenKey: string
+  updated?: IsoDateString
+  verified?: boolean
 }
 
 export type ParticipantsRecord = {
-	created?: IsoDateString
-	email?: string
-	id: string
-	nom?: string
-	prenom?: string
-	updated?: IsoDateString
+  created?: IsoDateString
+  email?: string
+  id: string
+  nom?: string
+  prenom?: string
+  updated?: IsoDateString
 }
 
 export type UsersRecord = {
-	avatar?: string
-	created?: IsoDateString
-	email: string
-	emailVisibility?: boolean
-	id: string
-	name?: string
-	password: string
-	tokenKey: string
-	updated?: IsoDateString
-	verified?: boolean
+  avatar?: string
+  created?: IsoDateString
+  email: string
+  emailVisibility?: boolean
+  id: string
+  name?: string
+  password: string
+  tokenKey: string
+  updated?: IsoDateString
+  verified?: boolean
 }
 
 // Response types include system fields and match responses from the PocketBase API
-export type AuthoriginsResponse<Texpand = unknown> = Required<AuthoriginsRecord> & BaseSystemFields<Texpand>
-export type ExternalauthsResponse<Texpand = unknown> = Required<ExternalauthsRecord> & BaseSystemFields<Texpand>
+export type AuthoriginsResponse<Texpand = unknown> = Required<AuthoriginsRecord> &
+  BaseSystemFields<Texpand>
+export type ExternalauthsResponse<Texpand = unknown> = Required<ExternalauthsRecord> &
+  BaseSystemFields<Texpand>
 export type MfasResponse<Texpand = unknown> = Required<MfasRecord> & BaseSystemFields<Texpand>
 export type OtpsResponse<Texpand = unknown> = Required<OtpsRecord> & BaseSystemFields<Texpand>
-export type SuperusersResponse<Texpand = unknown> = Required<SuperusersRecord> & AuthSystemFields<Texpand>
-export type ParticipantsResponse<Texpand = unknown> = Required<ParticipantsRecord> & BaseSystemFields<Texpand>
+export type SuperusersResponse<Texpand = unknown> = Required<SuperusersRecord> &
+  AuthSystemFields<Texpand>
+export type ParticipantsResponse<Texpand = unknown> = Required<ParticipantsRecord> &
+  BaseSystemFields<Texpand>
 export type UsersResponse<Texpand = unknown> = Required<UsersRecord> & AuthSystemFields<Texpand>
 
 // Types containing all Records and Responses, useful for creating typing helper functions
 
 export type CollectionRecords = {
-	_authOrigins: AuthoriginsRecord
-	_externalAuths: ExternalauthsRecord
-	_mfas: MfasRecord
-	_otps: OtpsRecord
-	_superusers: SuperusersRecord
-	participants: ParticipantsRecord
-	users: UsersRecord
+  _authOrigins: AuthoriginsRecord
+  _externalAuths: ExternalauthsRecord
+  _mfas: MfasRecord
+  _otps: OtpsRecord
+  _superusers: SuperusersRecord
+  participants: ParticipantsRecord
+  users: UsersRecord
 }
 
 export type CollectionResponses = {
-	_authOrigins: AuthoriginsResponse
-	_externalAuths: ExternalauthsResponse
-	_mfas: MfasResponse
-	_otps: OtpsResponse
-	_superusers: SuperusersResponse
-	participants: ParticipantsResponse
-	users: UsersResponse
+  _authOrigins: AuthoriginsResponse
+  _externalAuths: ExternalauthsResponse
+  _mfas: MfasResponse
+  _otps: OtpsResponse
+  _superusers: SuperusersResponse
+  participants: ParticipantsResponse
+  users: UsersResponse
 }
 
 // Type for usage with type asserted PocketBase instance
 // https://github.com/pocketbase/js-sdk#specify-typescript-definitions
 
 export type TypedPocketBase = PocketBase & {
-	collection(idOrName: '_authOrigins'): RecordService<AuthoriginsResponse>
-	collection(idOrName: '_externalAuths'): RecordService<ExternalauthsResponse>
-	collection(idOrName: '_mfas'): RecordService<MfasResponse>
-	collection(idOrName: '_otps'): RecordService<OtpsResponse>
-	collection(idOrName: '_superusers'): RecordService<SuperusersResponse>
-	collection(idOrName: 'participants'): RecordService<ParticipantsResponse>
-	collection(idOrName: 'users'): RecordService<UsersResponse>
+  collection(idOrName: '_authOrigins'): RecordService<AuthoriginsResponse>
+  collection(idOrName: '_externalAuths'): RecordService<ExternalauthsResponse>
+  collection(idOrName: '_mfas'): RecordService<MfasResponse>
+  collection(idOrName: '_otps'): RecordService<OtpsResponse>
+  collection(idOrName: '_superusers'): RecordService<SuperusersResponse>
+  collection(idOrName: 'participants'): RecordService<ParticipantsResponse>
+  collection(idOrName: 'users'): RecordService<UsersResponse>
 }
