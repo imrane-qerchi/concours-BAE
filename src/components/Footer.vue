902bea44dbae85d1126cafecd77bac064ceba301
Imrane QERCHI <150057046+imrane-qerchi@users.noreply.github.com>
2025-07-04 11:52:57 +0200

    Mise à jour du lien du logo dans le pied de page et suppression du bouton de contact

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,22 +3,12 @@
   <footer class="bg-[#742581] text-white py-8 px-6 w-full">
     <div class="max-w-7xl mx-auto flex flex-col lg:flex-row justify-between items-center gap-6">
       <!-- Logo à gauche -->
-      <a href="/" class="flex-shrink-0">
+      <a href="https://umlp.fr/" class="flex-shrink-0">
         <img src="/logo-UMLP-white.png" alt="Accueil" class="h-20 w-auto" />
       </a>
 
-      <!-- Bouton contact (mobile : en haut + bas marges) -->
-      <div class="flex-shrink-0 order-1 lg:order-3 mt-6 mb-6 lg:mt-0 lg:mb-0">
-        <a
-          href="mailto:contact-success-road@univ-fcomte.fr"
-          class="bg-white text-[#742581] text-xl px-6 py-3 rounded-xl hover:bg-gray-200 transition"
-        >
-          Contact
-        </a>
-      </div>
-
       <!-- Liens légaux -->
-      <div class="text-sm text-center lg:text-left order-2 lg:order-2">
+      <div class="text-sm text-center lg:text-left">
         <div class="flex gap-3 flex-wrap justify-center lg:justify-center mb-2">
           <RouterLink to="/mentions-legales" class="hover:underline">Mentions légales</RouterLink>
           <RouterLink to="/conditions-generales-d-utilisation" class="hover:underline"
