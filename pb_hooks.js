9a5e0b850e7c79b73c84380c3f269b83eeb82ab5
Imrane QERCHI <150057046+imrane-qerchi@users.noreply.github.com>
2025-09-04 14:39:36 +0200

    Réorganisation du code pour améliorer la lisibilité de la fonction de création d'enregistrement et ajout de logs pour le suivi des inscriptions.

--- pb_hooks.js
+++ pb_hooks.js
@@ -1,22 +1,23 @@
 onRecordCreateRequest(async (e) => {
-    e.next(); // Continue l'enregistrement du participant
+  console.log("➡️ Hook déclenché pour l'inscription :", e.record.get('email'))
+  e.next() // Continue l'enregistrement du participant
 
-    const prenom = e.record.get("prenom") || "participant";
-    const email = e.record.get("email");
+  const prenom = e.record.get('prenom') || 'participant'
+  const email = e.record.get('email')
 
-    if (!email) {
-        console.warn("Aucun email fourni, email non envoyé.");
-        return;
-    }
+  if (!email) {
+    console.warn('Aucun email fourni, email non envoyé.')
+    return
+  }
 
-    const message = new MailerMessage({
-        from: {
-            address: e.app.settings().meta.senderAddress,
-            name: e.app.settings().meta.senderName,
-        },
-        to: [{ address: email }],
-        subject: 'Merci pour ta participation au jeu concours "Success Road"',
-        html: `
+  const message = new MailerMessage({
+    from: {
+      address: e.app.settings().meta.senderAddress,
+      name: e.app.settings().meta.senderName
+    },
+    to: [{ address: email }],
+    subject: 'Merci pour ta participation au jeu concours "Success Road"',
+    html: `
             <p>Bonjour ${prenom},</p>
 
             <p>Nous avons bien pris en compte ton inscription au tirage au sort du Jeu concours <strong>Success Road</strong> organisé à l'occasion du festival <strong>Bienvenue aux étudiants</strong>.</p>
@@ -30,8 +31,8 @@ onRecordCreateRequest(async (e) => {
             <p>Nous te souhaitons bonne chance et une belle année universitaire !</p>
 
             <p>À bientôt,<br>L'équipe du réseau Alumni</p>
-        `,
-    });
+        `
+  })
 
-    await e.app.newMailClient().send(message);
-}, "participants"); // <== nom exact de ta collection
+  await e.app.newMailClient().send(message)
+}, 'participants') // <== nom exact de ta collection
